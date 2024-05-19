26540348f8e6caacaa73ec72526e10b02bb0a917
TheSam12 <osbel_torres@hotmail.com>
2024-05-18 22:58:28 -0400

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    './config/*.json', 
+    './layout/*.liquid', 
+    './assets/*.liquid', 
+    './sections/*.liquid', 
+    './snippets/*.liquid', 
+    './templates/*.liquid', 
+    './templates/*.json', 
+    './templates/customers/*.liquid', 
+    './templates/customers/*.json',
+  ],
+  fontFamily: {
+    'body': ['Montserrat'],
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['"Montserrat"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
+
