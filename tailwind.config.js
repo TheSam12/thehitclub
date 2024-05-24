0b71fd69fe88b0a9afb1b5c7721b8115d2b1d207
TheSam12 <osbel_torres@hotmail.com>
2024-05-23 22:19:47 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -75,14 +75,8 @@ module.exports = {
       80: '320px',
       96: '384px',
     },
-    fontSize: {
-      sm: '0.8rem',
-      base: '1rem',
-      xl: '1.25rem',
-      '2xl': '1.563rem',
-      '3xl': '1.953rem',
-      '4xl': '2.441rem',
-      '5xl': '3.052rem',
+    screens: {
+      '2xl': '1440px',
     },
   },
   plugins: [],
